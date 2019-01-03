e789a9f755377892f4ad392971d836a532c39ac6
Dharani3110 <dharanisara14@gmail.com>
2019-01-03 19:00:21 +0530

    Version 0.1.0

--- Face_recognition_SFR.py
+++ Face_recognition_SFR.py
@@ -8,6 +8,7 @@ import cv2
 import face_recognition
 import time
 import numpy as np
+from PIL import Image
 
 def recognise_face(test_image, train_images_encoded):
     """
@@ -17,25 +18,29 @@ def recognise_face(test_image, train_images_encoded):
     :param current_image_encoded:
     :return:
     """
-    cv2.imshow("test_image", test_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    image_to_be_matched = face_recognition.load_image_file(test_image)
-    image_to_be_matched_encoded = face_recognition.face_encodings(image_to_be_matched)[0]
-    print("\n\n\n\n",image_to_be_matched_encoded)
+    distances = []
+    cv2.imwrite("face"+".jpg", test_image)
+    test_image_path = 'face.jpg'
+    image_to_be_matched = face_recognition.load_image_file(test_image_path)
+    try:
+        image_to_be_matched_encoded = face_recognition.face_encodings(image_to_be_matched)[0]
 
-    for train_image_encoded in train_images_encoded:
+        for train_image_encoded in train_images_encoded:
         # compute distance between test image and train image
-        distance = face_recognition.face_distance([image_to_be_matched_encoded], train_image_encoded[0])
-        distances.append([distance[0], train_image_encoded[1]])
+            distance = face_recognition.face_distance([image_to_be_matched_encoded], train_image_encoded[0])
+            distances.append([distance[0], train_image_encoded[1]])
     # print(time.time() - start)
     #
     # #np.save(s,current_image_encoded )
     # # match your image with the image and check if it matches
     # #result = face_recognition.compare_faces( [image_to_be_matched_encoded], current_image_encoded , tolerance=0.67)
-    distances.sort(key=lambda x: x[0])
+        distances.sort(key=lambda x: x[0])
+        return distances[0][1]
+    except:
+        pass
+
    # print("\nTest image:",test_image)
-    return distances
+
 
 def main():
     """
@@ -43,9 +48,12 @@ def main():
     :return:
     """
     print("------------------ Soliton Face Recognition ------------------\n\n")
+
     #Make the path relative
     train_image_encoded = []
     images = os.listdir('/home/soliton/work/SFR/dev/SFR_code/sample_train_images/')
+
+
     '''
     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
     cv2.imshow('image',image_to_be_matched)
@@ -57,29 +65,38 @@ def main():
     #Explain what this xml file is
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     #compute the encodings for all train_images
+    print(len(images))
     for image in images:
         (image_name,ext)= os.path.splitext(image)
         # load the image
         train_img = face_recognition.load_image_file("sample_train_images/" + image)
         # encode the loaded image into a feature vector
         train_image_encoded.append([face_recognition.face_encodings(train_img)[0], image_name])
-
+    print("here")
     # start = time.time()
     while True:
         # load your image
         ret, test_image = cap.read()
+
         img = test_image
         if ret:
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             equ = cv2.equalizeHist(gray)
-            faces = face_cascade.detectMultiScale(img, 1.3, 5)
+            res = np.hstack((gray, equ))
+
+
+            faces = face_cascade.detectMultiScale(res, 1.3, 5)
             for (x, y, w, h) in faces:
                 cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv2.imshow("test_image", img)
-            if cv2.waitKey(30) & 0xff == ord('q'):
+
+
+                if (len(faces) != 0):
+                    text = recognise_face(test_image, train_image_encoded)
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    cv2.putText(test_image, text, (x,y), font, 2, (255, 0, 0), 2, cv2.LINE_AA)
+            cv2.imshow("Find Your Face :P", test_image)
+            if cv2.waitKey(100) & 0xff == ord('q'):
                 break
-            if (len(faces) != 0):
-                recognise_face(test_image, train_image_encoded)
             else:
                 continue
 
